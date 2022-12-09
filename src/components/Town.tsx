6b59dd28112176bdee43efc4760326802189cced
0-Chan <kyc4421194@gmail.com>
2022-12-09 14:00:03 +0900

    Add Town component

---
+++ src/components/Town.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+import wheelImage from '../assets/wheel.png';
+import townDayImage from '../assets/town_day.png';
+import townNightImage from '../assets/town_night.png';
+
+const Wheel = styled.img`
+  position: absolute;
+  bottom: 104px;
+  left: 50%;
+  margin-left: -165px;
+  opacity: 0.7;
+`;
+const TownDay = styled.img`
+  position: absolute;
+  bottom: 0px;
+  left: 40px;
+`;
+const TownNight = styled.img`
+  position: absolute;
+  bottom: 0px;
+  left: 40px;
+`;
+
+export default function Town() {
+  return (
+    <>
+      <Wheel src={wheelImage} />
+      <TownDay src={townDayImage} />
+      <TownNight src={townNightImage} />
+    </>
+  );
+}
