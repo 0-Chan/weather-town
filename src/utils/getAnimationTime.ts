c5c0fd060c112575b601533f898231f738ef56b7
0-Chan <kyc4421194@gmail.com>
2022-12-10 01:07:10 +0900

    Add exception handling for getAnimationTime

--- src/utils/getAnimationTime.ts
+++ src/utils/getAnimationTime.ts
@@ -1,6 +1,11 @@
 /* eslint-disable import/prefer-default-export */
-export function getAnimationTime(animation: string, hours: string) {
-  const time = Math.floor(Number(hours) / 3).toString();
+export function getAnimationTime(animation: string, hours: number) {
+  const time = Math.floor(hours / 3).toString();
+
+  if (time === '0') {
+    // 0~23 시간제라 0시 일 때의 예외처리
+    return animation + 8;
+  }
 
   return animation + time;
 }
