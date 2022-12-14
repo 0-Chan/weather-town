dabd8c6ce538499ca8be6a40796f293394608b28
0-Chan <kyc4421194@gmail.com>
2022-12-14 23:59:58 +0900

    Add some weather related models

---
+++ src/models/Weather/Data.ts
@@ -0,0 +1,32 @@
+import { Coords } from '../Coords';
+import { WeatherCodeType } from './Types';
+import {
+  Weather,
+  Main,
+  Clouds,
+  Sys,
+  Wind,
+} from './Props';
+
+export interface WeatherDataResponse {
+  coord: Coords;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+export interface WeatherData {
+  weather: Weather;
+  city: string;
+  country: string;
+  temp: number;
+  type: WeatherCodeType;
+}
